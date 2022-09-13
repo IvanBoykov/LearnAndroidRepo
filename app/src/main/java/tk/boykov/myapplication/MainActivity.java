e171c2d38450263e174a11482e0c45ad2afd4353
Иван Бойков <ivan.bojkov.904@gmail.com>
2022-09-13 14:21:06 +0600

    Hello kitty v1

--- app/src/main/java/tk/boykov/myapplication/MainActivity.java
+++ app/src/main/java/tk/boykov/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package tk.boykov.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+	private TextView textView;
+	private EditText kittyName;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		textView = findViewById(R.id.TextView);
+		kittyName = findViewById(R.id.EditKittyName);
+	}
+	
+	public void onClickButton1(View view) {
+		if (kittyName.getText().length() == 0) {
+			textView.setText("Hello Kitty!");
+		} else {
+			textView.setText("Привет, " + kittyName.getText());
+		}
 	}
 }
